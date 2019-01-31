2453018714a06f476b6abcab0d0190c0f097db22
Alexandre Six Belem <alexandrenilton@gmail.com>
2019-01-30 23:49:10 -0200

    Error in /books url

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -16,6 +16,7 @@ public class BookController {
 
     @RequestMapping("/books")
     public String getBooks(Model model) {
+        System.out.println("getBooks called!");
         model.addAttribute("books", bookRepository.findAll());
 
         return "books";
